df3ffbefb66355e446ca961d945897922d23ea56
marta.liang.gutierrez <marta.liang.gutierrez@auleseebe.upc.edu>
2024-09-19 11:41:34 +0200

    ...

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,12 @@
 #Codigo para problema 2
+
+ls() #listar todas la variables hasta el momento
+rm(lsit=ls()) #borrar todos los datos que había guardado antes
+
+data = mtcars
+data
+
+hist(data$mpg) #Respuesta Pbm2
+
+pie(data$mpg)
+pie(table(data$mpg))
